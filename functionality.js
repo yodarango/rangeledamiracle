16f396a9e3e1002fff4440ebb1565374b098ca60
yodarango <danranngel@gmail.com>
2020-08-12 07:25:07 -0400

    added the counter circle finally

--- functionality.js
+++ functionality.js
@@ -14,10 +14,19 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+  let dasharray = days * 2.3;
+  let dasharray2 = days * 3;
+  let dasharray3 = days * 4;
   // Display the result in the element with id="countdown"
   document.getElementById("countdown").textContent = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
+
+  document.querySelector("#circle")
+    .style.setProperty("stroke-dasharray", dasharray);
+    document.querySelector("#circle2")
+    .style.setProperty("stroke-dasharray", dasharray2);
+    document.querySelector("#circle3")
+    .style.setProperty("stroke-dasharray", dasharray3);
 
   // If the count down is finished, write some text 
   if (distance < 0) {
@@ -25,3 +34,5 @@ var x = setInterval(function() {
     document.getElementById("countdown").textContent = "We said \"I do\"";
   }
 }, 1000);
+
+  
---
+++ lang.js
@@ -0,0 +1,54 @@
+function language() {
+    let a = document.querySelector("#bride");
+    let b = document.querySelector("#story");
+    let c = document.querySelector("#groom");
+    let d = document.querySelector("#ceremony");
+    let e = document.querySelector("#date");
+    let f = document.querySelector(".get-directions");
+    let g = document.querySelector("#reception");
+    let h = document.querySelector("#address2");
+    let i = document.querySelector("#get-directions");
+    let j = document.querySelector("#add-det");
+    let k = document.querySelector("#page1");
+    let l = document.querySelector("#page2");
+    let m = document.querySelector("#page3");
+    let n = document.querySelector("#page4");
+  
+    if (document.querySelector(".checked").checked) {
+      a.innerHTML = "LA NOVIA";
+      b.innerHTML = "COMO PASO";
+      c.innerHTML = "EL NOVIO";
+      d.innerHTML = "CEREMONIA";
+      e.outerHTML =
+        '<p id="date"><span class="date">Junio 19, 2021 6:30</span><br>123 una calle <br>Hot Springs, AK 37777</p>';
+      f.innerHTML = "Como Llegar";
+      g.innerHTML = "RECEPCION";
+      h.outerHTML =
+        '<p id="address2"><span class="date">Junio 19, 2021 Terminando La Ceremonia</span><br> 123 Una Calle <br> Hot Springs, AK 37777</p>';
+      i.innerHTML = "Como Llegar";
+      j.innerHTML = "MAS DETALLES";
+      k.innerHTML = "Como Va A Ser";
+      l.innerHTML = "Hospedaje";
+      m.outerHTML = '<a id="page3" href="#">Attracciones en<br>Hot Springs</a>';
+      n.innerHTML = "Regalar";
+    } else {
+      a.innerHTML = "THE BRIDE";
+      b.innerHTML = "OUR STORY";
+      c.innerHTML = "THE GROOM";
+      d.innerHTML = "CEREMONY";
+      e.outerHTML =
+        '<p id="date"><span class="date">June 19th, 2021 6:30</span><br>123 Some Street <br>Hot Springs, AK 37777</p>';
+      f.innerHTML = "Get Directions";
+      g.innerHTML = "RECEPTION";
+      h.outerHTML =
+        '<p id="address2"><span class="date">June 19th, 2021 Following Ceremony</span><br> 123 Some Street <br> Hot Springs, AK 37777</p>';
+      i.innerHTML = "Como Llegar";
+      j.innerHTML = "ADDITIONAL DETAILS";
+      k.innerHTML = "What To Expect";
+      l.innerHTML = "Lodging";
+      m.outerHTML = '<a id="page3" href="#">Things To Do <br>In Hot Springs</a>';
+      n.innerHTML = "Registry";
+    }
+  }
+  
+  
